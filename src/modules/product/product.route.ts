df4ca090982a8c43c6a2bebf70c7edbe5fe4787e
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2024-11-24 00:41:09 +0600

    get all stationary product funtionality added

--- src/modules/product/product.route.ts
+++ src/modules/product/product.route.ts
@@ -4,5 +4,6 @@ import { ProductController } from './product.controller';
 const productRouter = Router();
 
 productRouter.post('/', ProductController.createStationaryProduct);
+productRouter.get('/', ProductController.getAllStationaryProducts);
 
 export default productRouter;
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -2,10 +2,16 @@ import { IProduct } from './product.interface';
 import Product from './product.model';
 
 const createStationaryProductIntoDB = async (productData: IProduct) => {
-  const result = await Product.create(productData);
-  return result;
+    const result = await Product.create(productData);
+    return result;
 };
 
-export const ProductService = {
-  createStationaryProductIntoDB,
+const getAllStationaryProductsFromDB = async () => {
+    const result = await Product.find();
+    return result;
+}
+
+export const ProductServices = {
+    createStationaryProductIntoDB,
+    getAllStationaryProductsFromDB
 };
